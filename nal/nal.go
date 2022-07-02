9a2bbe6549a17d46b88e132225336a9d6e724496
Goya <fe.goya@gmail.com>
2022-07-02 11:06:03 +0000

    reimplent a few h264 nal handling

---
+++ nal/nal.go
@@ -0,0 +1,113 @@
+package nal
+
+import "bytes"
+
+// Same as binary.BigEndian.Uint32()
+// func next32(b []byte) uint32 {
+// 	_ = b[3] // bounds check hint to compiler; see golang.org/issue/14808
+// 	return uint32(b[3]) | uint32(b[2])<<8 | uint32(b[1])<<16 | uint32(b[0])<<24
+// }
+
+// func next24(b []byte) uint32 {
+// 	_ = b[2] // bounds check hint to compiler; see golang.org/issue/14808
+// 	return uint32(b[2]) | uint32(b[1])<<8 | uint32(b[0])<<16
+// }
+
+type Unit []byte
+
+// forbidden_zero_bit.  A value of 0 indicates that the NAL unit
+// type octet and payload should not contain bit errors or other
+// syntax violations.  A value of 1 indicates that the NAL unit
+// type octet and payload may contain bit errors or other syntax
+// violations.
+func (u Unit) F() bool {
+	return u[0]&0x80 != 0x00
+}
+
+// nal_ref_idc.  The semantics of value 00 and a non-zero value
+// remain unchanged from the H.264 specification.  In other words,
+// a value of 00 indicates that the content of the NAL unit is not
+// used to reconstruct reference pictures for inter picture
+// prediction.  Such NAL units can be discarded without risking
+// the integrity of the reference pictures.  Values greater than
+// 00 indicate that the decoding of the NAL unit is required to
+// maintain the integrity of the reference pictures.
+func (u Unit) NRI() byte {
+	return u[0] >> 5 & 0x03
+}
+
+// nal_unit_type
+func (u Unit) Type() byte {
+	return u[0] & 0x1F
+}
+
+// Raw Byte Sequence Payload
+func (u Unit) Payload() []byte {
+	return u[1:]
+}
+
+func (u Unit) IsZero() bool {
+	return len(u) == 0
+}
+
+// Byte stream splitter
+func AnnexBSplit(b []byte) (units []Unit, leading bool) {
+
+	// Not sure if it is faster this way
+	r := bytes.Split(b, []byte{0x00, 0x00, 0x01})
+	if len(r) == 0 { // I don't think this happens
+		return nil, false
+	}
+
+	res := make([]Unit, 0, len(r))
+	for i, last := 0, len(r)-1; i <= last; i++ {
+		v := r[i]
+		if i != last && len(v) > 0 && v[len(v)-1] == 0x00 {
+			res = append(res, Unit(v[:len(v)-1]))
+		} else {
+			res = append(res, Unit(v))
+		}
+	}
+
+	if len(res[0]) == 0 {
+		return res[1:], true
+	}
+	return res, false
+
+	// var res []Unit
+	// i := 0
+	// for i < len(b) {
+	// 	if n := len(b) - i; n >= 4 {
+	// 		if next32(b[i:]) == 0x0000001 {
+	// 			if i > 0 {
+	// 				res = append(res, Unit(b[:i]))
+	// 			}
+	// 			b = b[i+4:]
+	// 			i = 0
+	// 			continue
+	// 		} else if next24(b[i:]) == 0x00001 {
+	// 			if i > 0 {
+	// 				res = append(res, Unit(b[:i]))
+	// 			}
+	// 			b = b[i+3:]
+	// 			i = 0
+	// 			continue
+	// 		}
+	// 	} else if n == 3 {
+	// 		if next24(b[i:]) == 0x00001 {
+	// 			if i > 0 {
+	// 				res = append(res, Unit(b[:i]))
+	// 			}
+	// 			b = b[i+3:]
+	// 			i = 0
+	// 			continue
+	// 		}
+	// 	}
+	// 	i++
+	// }
+	// if i > 0 {
+	// 	res = append(res, Unit(b[:i]))
+	// }
+
+	// return res
+}
--- stream2.go
+++ stream2.go
@@ -9,6 +9,7 @@ import (
 	"math"
 	"time"
 
+	"github.com/deepch/RTSPtoWebRTC/nal"
 	"github.com/deepch/RTSPtoWebRTC/rtsp"
 	"github.com/deepch/vdk/av"
 	"github.com/deepch/vdk/codec"
@@ -23,7 +24,7 @@ type RTSPStream struct {
 	AudioOnly bool
 	keyTest   *time.Timer
 
-	fuStarted       bool
+	// fuStarted       bool
 	BufferRtpPacket *bytes.Buffer
 	// startVideoTS    int64
 	// startAudioTS    int64
@@ -169,19 +170,26 @@ func (s *RTSPStream) onVideoPacket(player *rtsp.Player, p *rtp.Packet) error {
 		s.BufferRtpPacket.Truncate(0)
 		s.BufferRtpPacket.Reset()
 	}
-	nalRaw, _ := h264parser.SplitNALUs(p.Payload)
-	if len(nalRaw) == 0 || len(nalRaw[0]) == 0 {
+
+	// This don't make too much sense to me. I believe since it is RTP packets it
+	// shouldn't use Byte Stream, maybe some bad cameras do it?
+	// nalRaw, _ := h264parser.SplitNALUs(p.Payload)
+	nalus := []nal.Unit{p.Payload}
+	if len(nalus) == 0 || len(nalus[0]) == 0 {
 		// return nil, false
-		return fmt.Errorf("len(nalRaw) == 0 || len(nalRaw[0]) == 0")
+		return fmt.Errorf("len(nalus) == 0 || len(nalus[0]) == 0")
+	}
+	if t := nalus[0].Type(); t == 7 || t == 9 {
+		nalus, _ = nal.AnnexBSplit(nalus[0])
 	}
 	var retmap []*av.Packet
-	for _, nal := range nalRaw {
+	for _, nalu := range nalus {
 		if s.videoCodec == av.H265 {
-			naluType := (nal[0] >> 1) & 0x3f
+			naluType := (nalu[0] >> 1) & 0x3f
 			switch naluType {
 			case h265parser.NAL_UNIT_CODED_SLICE_TRAIL_R:
 				retmap = append(retmap, &av.Packet{
-					Data:            binSize(nal),
+					Data:            binSize(nalu),
 					CompositionTime: time.Duration(1) * time.Millisecond,
 					Idx:             s.videoIDX,
 					IsKeyFrame:      false,
@@ -189,24 +197,24 @@ func (s *RTSPStream) onVideoPacket(player *rtsp.Player, p *rtp.Packet) error {
 					Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
 				})
 			case h265parser.NAL_UNIT_VPS:
-				s.CodecUpdateVPS(nal)
+				s.CodecUpdateVPS(nalu)
 			case h265parser.NAL_UNIT_SPS:
-				s.CodecUpdateSPS(nal)
+				s.CodecUpdateSPS(nalu)
 			case h265parser.NAL_UNIT_PPS:
-				s.CodecUpdatePPS(nal)
+				s.CodecUpdatePPS(nalu)
 			case h265parser.NAL_UNIT_UNSPECIFIED_49:
-				se := nal[2] >> 6
-				naluType := nal[2] & 0x3f
+				se := nalu[2] >> 6
+				naluType := nalu[2] & 0x3f
 				if se == 2 {
 					s.BufferRtpPacket.Truncate(0)
 					s.BufferRtpPacket.Reset()
-					s.BufferRtpPacket.Write([]byte{(nal[0] & 0x81) | (naluType << 1), nal[1]})
+					s.BufferRtpPacket.Write([]byte{(nalu[0] & 0x81) | (naluType << 1), nalu[1]})
 					r := make([]byte, 2)
-					r[1] = nal[1]
-					r[0] = (nal[0] & 0x81) | (naluType << 1)
-					s.BufferRtpPacket.Write(nal[3:])
+					r[1] = nalu[1]
+					r[0] = (nalu[0] & 0x81) | (naluType << 1)
+					s.BufferRtpPacket.Write(nalu[3:])
 				} else if se == 1 {
-					s.BufferRtpPacket.Write(nal[3:])
+					s.BufferRtpPacket.Write(nalu[3:])
 					retmap = append(retmap, &av.Packet{
 						Data:            binSize(s.BufferRtpPacket.Bytes()),
 						CompositionTime: time.Duration(1) * time.Millisecond,
@@ -216,114 +224,181 @@ func (s *RTSPStream) onVideoPacket(player *rtsp.Player, p *rtp.Packet) error {
 						Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
 					})
 				} else {
-					s.BufferRtpPacket.Write(nal[3:])
+					s.BufferRtpPacket.Write(nalu[3:])
 				}
 			default:
 				log.Println("Unsupported Nal", naluType)
 			}
 
 		} else if s.videoCodec == av.H264 {
-			naluType := nal[0] & 0x1f
-			switch {
-			case naluType >= 1 && naluType <= 5:
-				if naluType != 1 {
-					log.Println("naluType >= 1 && naluType <= 5:", naluType)
-				}
+			switch nalu.Type() { // unsigned integer using 5 bits
+			case 1, 2, 3, 4: // VCL
 				retmap = append(retmap, &av.Packet{
-					Data:            binSize(nal),
+					Data:            binSize(nalu),
 					CompositionTime: time.Duration(1) * time.Millisecond,
 					Idx:             s.videoIDX,
-					IsKeyFrame:      naluType == 5,
+					IsKeyFrame:      false,
 					Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
 					Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
 				})
-			case naluType == 7:
-				// log.Println("naluType == 7")
-				s.CodecUpdateSPS(nal)
-				if len(nal) > 30 {
-					log.Printf("naluType == 7 Packet: %#v", p)
-				}
-			case naluType == 8:
-				// log.Println("naluType == 8")
-				s.CodecUpdatePPS(nal)
-			case naluType == 24:
-				packet := nal[1:]
-				for len(packet) >= 2 {
-					size := int(packet[0])<<8 | int(packet[1])
-					if size+2 > len(packet) {
+			case 5:
+				retmap = append(retmap, &av.Packet{
+					Data:            binSize(nalu),
+					CompositionTime: time.Duration(1) * time.Millisecond,
+					Idx:             s.videoIDX,
+					IsKeyFrame:      true,
+					Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
+					Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
+				})
+			case 7: // Sequence parameter set
+				s.CodecUpdateSPS(nalu)
+			case 8: // Picture parameter set
+				s.CodecUpdatePPS(nalu)
+			case 24: // RFC6184: STAP-A Single-time aggregation packet
+				b := nalu.Payload()
+				for len(b) >= 2 {
+					size := int(b[1]) | int(b[0])<<8
+					if size == 0 || len(b) < 2+size {
+						log.Println("incorrect packet size in nal_unit_type 24")
 						break
 					}
-					naluTypefs := packet[2] & 0x1f
-					switch {
-					case naluTypefs >= 1 && naluTypefs <= 5:
+					nalu := nal.Unit(b[2 : size+2])
+					switch nalu.Type() {
+					case 1, 2, 3, 4:
 						retmap = append(retmap, &av.Packet{
-							Data:            binSize(packet[2 : size+2]),
+							Data:            binSize(nalu),
 							CompositionTime: time.Duration(1) * time.Millisecond,
 							Idx:             s.videoIDX,
-							IsKeyFrame:      naluType == 5,
+							IsKeyFrame:      false,
 							Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
 							Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
 						})
-					case naluTypefs == 7:
-						s.CodecUpdateSPS(packet[2 : size+2])
-					case naluTypefs == 8:
-						s.CodecUpdatePPS(packet[2 : size+2])
+					case 5:
+						retmap = append(retmap, &av.Packet{
+							Data:            binSize(nalu),
+							CompositionTime: time.Duration(1) * time.Millisecond,
+							Idx:             s.videoIDX,
+							IsKeyFrame:      true,
+							Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
+							Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
+						})
+					case 7:
+						s.CodecUpdateSPS(nalu)
+					case 8:
+						s.CodecUpdatePPS(nalu)
+					default:
+						log.Println("24: Unsupported NAL Type", nalu.Type())
 					}
-					packet = packet[size+2:]
+					b = b[2+size:]
 				}
-			case naluType == 28:
-				fuIndicator := p.Payload[0]
-				fuHeader := p.Payload[1]
-				isStart := fuHeader&0x80 != 0
-				isEnd := fuHeader&0x40 != 0
-				// log.Println("naluType == 28:", fuIndicator, fuHeader, isStart, isEnd)
-				if isStart {
-					s.fuStarted = true
-					s.BufferRtpPacket.Truncate(0)
+			case 28: // RFC6184: FU-A Fragmentation unit
+				// nalu[0] is the fragmentation unit indicator
+				// nalu[1] is the fragmentation unit header
+				// first bit of the header is start
+				// second bit of the header is end
+				// third bit of the header is reserved
+				// remaining five bits is the actual nal_unit_type
+				start := nalu[1]&0x80 != 0
+				end := nalu[1]&0x40 != 0
+				if start {
 					s.BufferRtpPacket.Reset()
-					s.BufferRtpPacket.Write([]byte{fuIndicator&0xe0 | fuHeader&0x1f})
+					s.BufferRtpPacket.WriteByte((nalu[0] & 0xE0) | (nalu[1] & 0x1F)) // replace 5 bit nal_unit_type
 				}
-				if s.fuStarted {
-					if !bytes.Equal(p.Payload[2:], p.Payload[2:len(p.Payload)]) {
-						log.Println("FIX: Reslicing payload, not sure this is the correct way at player.go:475")
+				if s.BufferRtpPacket.Len() > 0 {
+					s.BufferRtpPacket.Write(nalu[2:])
+					if !end {
+						break
 					}
-					s.BufferRtpPacket.Write(p.Payload[2:])
-					if isEnd {
-						s.fuStarted = false
-						naluTypef := s.BufferRtpPacket.Bytes()[0] & 0x1f
-						if naluTypef == 7 || naluTypef == 9 {
-							bufered, _ := h264parser.SplitNALUs(append([]byte{0, 0, 0, 1}, s.BufferRtpPacket.Bytes()...))
-							for _, v := range bufered {
-								naluTypefs := v[0] & 0x1f
-								switch {
-								case naluTypefs == 5:
-									// log.Println("naluTypefs == 5")
-									s.BufferRtpPacket.Reset()
-									s.BufferRtpPacket.Write(v)
-									naluTypef = 5
-								case naluTypefs == 7:
-									// log.Println("naluTypefs == 7")
-									// log.Println("SPS2", base64.StdEncoding.EncodeToString(v), v)
-									s.CodecUpdateSPS(v)
-								case naluTypefs == 8:
-									// log.Println("naluTypefs == 8")
-									// log.Println("PPS2", base64.StdEncoding.EncodeToString(v), v)
-									s.CodecUpdatePPS(v)
-								}
-							}
+					nalus := []nal.Unit{s.BufferRtpPacket.Bytes()}
+					s.BufferRtpPacket.Reset()
+					// RFC6184 seems to not allow Annex B, but my camera work like this
+					if t := nalus[0].Type(); t == 7 || t == 9 {
+						nalus, _ = nal.AnnexBSplit(nalus[0])
+					}
+					for _, nalu := range nalus {
+						switch nalu.Type() {
+						case 1, 2, 3, 4:
+							retmap = append(retmap, &av.Packet{
+								Data:            binSize(nalu),
+								CompositionTime: time.Duration(1) * time.Millisecond,
+								Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
+								Idx:             s.videoIDX,
+								IsKeyFrame:      false,
+								Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
+							})
+						case 5:
+							retmap = append(retmap, &av.Packet{
+								Data:            binSize(nalu),
+								CompositionTime: time.Duration(1) * time.Millisecond,
+								Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
+								Idx:             s.videoIDX,
+								IsKeyFrame:      true,
+								Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
+							})
+						case 7: // Sequence parameter set
+							s.CodecUpdateSPS(nalu)
+						case 8: // Picture parameter set
+							s.CodecUpdatePPS(nalu)
+						case 12: // I got a few of these, not sure what to do yet
+						default:
+							log.Println("28: Unsupported NAL Type", nalu.Type())
 						}
-						retmap = append(retmap, &av.Packet{
-							Data:            binSize(s.BufferRtpPacket.Bytes()),
-							CompositionTime: time.Duration(1) * time.Millisecond,
-							Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
-							Idx:             s.videoIDX,
-							IsKeyFrame:      naluTypef == 5,
-							Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
-						})
 					}
 				}
+
+				// fuIndicator := p.Payload[0]
+				// fuHeader := p.Payload[1]
+				// isStart := fuHeader&0x80 != 0
+				// isEnd := fuHeader&0x40 != 0
+				// if isStart {
+				// 	s.fuStarted = true
+				// 	// s.BufferRtpPacket.Truncate(0)
+				// 	s.BufferRtpPacket.Reset()
+				// 	s.BufferRtpPacket.Write([]byte{fuIndicator&0xe0 | fuHeader&0x1f})
+				// }
+				// if s.fuStarted {
+				// 	s.BufferRtpPacket.Write(p.Payload[2:])
+				// 	if isEnd {
+				// 		s.fuStarted = false
+				// 		payload := s.BufferRtpPacket.Bytes()
+				// 		naluTypef := payload[0] & 0x1f
+				// 		if naluTypef == 7 || naluTypef == 9 {
+				// 			log.Println("naluTypef == 7 || naluTypef == 9")
+				// 			// bufered, _ := h264parser.SplitNALUs(append([]byte{0, 0, 0, 1}, s.BufferRtpPacket.Bytes()...))
+				// 			bufered := nal2.AnnexBSplit(payload)
+				// 			for _, v := range bufered {
+				// 				naluTypefs := v[0] & 0x1f
+				// 				switch {
+				// 				case naluTypefs == 5:
+				// 					// log.Println("naluTypefs == 5")
+				// 					s.BufferRtpPacket.Reset()
+				// 					s.BufferRtpPacket.Write(v)
+				// 					naluTypef = 5
+				// 				case naluTypefs == 7:
+				// 					// log.Println("naluTypefs == 7")
+				// 					// log.Println("SPS2", base64.StdEncoding.EncodeToString(v), v)
+				// 					s.CodecUpdateSPS(v)
+				// 				case naluTypefs == 8:
+				// 					// log.Println("naluTypefs == 8")
+				// 					// log.Println("PPS2", base64.StdEncoding.EncodeToString(v), v)
+				// 					s.CodecUpdatePPS(v)
+				// 				default:
+				// 					log.Println("28: Unsupported NAL Type", naluTypefs, len(bufered), naluTypef)
+				// 				}
+				// 			}
+				// 		}
+				// 		retmap = append(retmap, &av.Packet{
+				// 			Data:            binSize(s.BufferRtpPacket.Bytes()),
+				// 			CompositionTime: time.Duration(1) * time.Millisecond,
+				// 			Duration:        time.Duration(float32(int64(p.Timestamp)-s.PreVideoTS)/90) * time.Millisecond,
+				// 			Idx:             s.videoIDX,
+				// 			IsKeyFrame:      naluTypef == 5,
+				// 			Time:            time.Duration(p.Timestamp/90) * time.Millisecond,
+				// 		})
+				// 	}
+				// }
 			default:
-				log.Println("Unsupported NAL Type", naluType)
+				log.Println("Unsupported NAL Type", nalu.Type())
 			}
 		}
 	}
@@ -485,16 +560,16 @@ func (s *RTSPStream) onAudioPacket(player *rtsp.Player, p *rtp.Packet) error {
 	if s.PreAudioTS == 0 {
 		s.PreAudioTS = int64(p.Timestamp)
 	}
-	nalRaw, _ := h264parser.SplitNALUs(p.Payload)
+	nalus, _ := h264parser.SplitNALUs(p.Payload)
 	var retmap []*av.Packet
-	for _, nal := range nalRaw {
+	for _, nalu := range nalus {
 		var duration time.Duration
 		switch s.audioCodec {
 		case av.PCM_MULAW:
-			duration = time.Duration(len(nal)) * time.Second / time.Duration(s.AudioTimeScale)
+			duration = time.Duration(len(nalu)) * time.Second / time.Duration(s.AudioTimeScale)
 			s.AudioTimeLine += duration
-			data := make([]byte, len(nal))
-			copy(data, nal)
+			data := make([]byte, len(nalu))
+			copy(data, nalu)
 			retmap = append(retmap, &av.Packet{
 				Data:            data,
 				CompositionTime: time.Duration(1) * time.Millisecond,
@@ -504,10 +579,10 @@ func (s *RTSPStream) onAudioPacket(player *rtsp.Player, p *rtp.Packet) error {
 				Time:            s.AudioTimeLine,
 			})
 		case av.PCM_ALAW:
-			duration = time.Duration(len(nal)) * time.Second / time.Duration(s.AudioTimeScale)
+			duration = time.Duration(len(nalu)) * time.Second / time.Duration(s.AudioTimeScale)
 			s.AudioTimeLine += duration
-			data := make([]byte, len(nal))
-			copy(data, nal)
+			data := make([]byte, len(nalu))
+			copy(data, nalu)
 			retmap = append(retmap, &av.Packet{
 				Data:            data,
 				CompositionTime: time.Duration(1) * time.Millisecond,
@@ -519,8 +594,8 @@ func (s *RTSPStream) onAudioPacket(player *rtsp.Player, p *rtp.Packet) error {
 		case av.OPUS:
 			duration = time.Duration(20) * time.Millisecond
 			s.AudioTimeLine += duration
-			data := make([]byte, len(nal))
-			copy(data, nal)
+			data := make([]byte, len(nalu))
+			copy(data, nalu)
 			retmap = append(retmap, &av.Packet{
 				Data:            data,
 				CompositionTime: time.Duration(1) * time.Millisecond,
@@ -530,11 +605,11 @@ func (s *RTSPStream) onAudioPacket(player *rtsp.Player, p *rtp.Packet) error {
 				Time:            s.AudioTimeLine,
 			})
 		case av.AAC:
-			auHeadersLength := uint16(0) | (uint16(nal[0]) << 8) | uint16(nal[1])
+			auHeadersLength := uint16(0) | (uint16(nalu[0]) << 8) | uint16(nalu[1])
 			auHeadersCount := auHeadersLength >> 4
 			framesPayloadOffset := 2 + int(auHeadersCount)<<1
-			auHeaders := nal[2:framesPayloadOffset]
-			framesPayload := nal[framesPayloadOffset:]
+			auHeaders := nalu[2:framesPayloadOffset]
+			framesPayload := nalu[framesPayloadOffset:]
 			for i := 0; i < int(auHeadersCount); i++ {
 				auHeader := uint16(0) | (uint16(auHeaders[0]) << 8) | uint16(auHeaders[1])
 				frameSize := auHeader >> 3
